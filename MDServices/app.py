5876a3049fa18088747e6e810e0df417cfd9432c
ThomaxIgnition <113600602+ThomaxIgnition@users.noreply.github.com>
2024-01-09 15:40:55 +0100

    Update app.py

--- MDServices/app.py
+++ MDServices/app.py
@@ -9,3 +9,13 @@ def home():
 
 if __name__ == '__main__':
     app.run(debug=True)
+# app/__init__.py
+
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+csrf = CSRFProtect(app)
+
+from app import routes
