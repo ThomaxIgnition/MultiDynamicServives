45bd110c9db696bb1173b31c226a6c9c2111e740
ThomaxIgnition <113600602+ThomaxIgnition@users.noreply.github.com>
2024-01-09 16:26:28 +0100

    Update app.py

--- MDServices/app.py
+++ MDServices/app.py
@@ -1,5 +1,8 @@
 # app.py
 from flask import Flask, render_template
+from flask_login import LoginManager
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 
@@ -10,9 +13,6 @@ def home():
 if __name__ == '__main__':
     app.run(debug=True)
 # app/__init__.py
-
-from flask_login import LoginManager
-from flask_wtf.csrf import CSRFProtect
 
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
